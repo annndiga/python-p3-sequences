40c39f8c15ae666fcfa536cda446a3a71cb09470
Ann Ndiga <annndiga@gmail.com>
2023-08-18 19:15:54 +0300

    Done assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_list = []
+    a, b = 0, 1
+
+    for _ in range(length):
+        fibonacci_list.append(a)
+        a, b = b, a + b
+
+    print(fibonacci_list)
+
+print_fibonacci(9)
